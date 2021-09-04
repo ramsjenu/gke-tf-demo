f9498e13800b7de41a8f21c4e90c5831dc06e3b7
ramsjenu <vramschennai@gmail.com>
2021-09-04 08:47:12 +0100

    11th commit

--- gkecluster.tf
+++
@@ -1,6 +0,0 @@
-resource "google_container_cluster" "gke-cluster" {
-  name               = "my-second-gke-cluster"
-  network            = "default"
-  zone               = "europe-west1-a"
-  initial_node_count = 3
-}
